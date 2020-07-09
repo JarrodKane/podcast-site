f3645a44e7ac36199241255efd5a7be1d7119451
Trojan-Cat <kajarrod@gmail.com>
2020-07-09 11:37:35 +1000

    Episode card now has flex and is responsive

--- src/components/episodecard.js
+++ src/components/episodecard.js
@@ -15,14 +15,35 @@ const Card = styled.div`
   flex-direction: column;
   padding: 1rem;
 `
+const EpMain = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-flow: row wrap;
+`
+const EpContent = styled.div`
+  padding: 0.5rem;
+  @media (min-width: 600px) {
+    width: 50%;
+  }
+`
 
 const EpisodeCard = () => (
   <Card>
     <EpisodeCardHeader />
-    <div>
-      <div>Card Text</div>
-      <div>Card Socials and icon</div>
-    </div>
+    <EpMain>
+      <EpContent>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eleifend
+        at risus et congue. Donec consectetur lorem eget nunc aliquam, eget
+        bibendum metus posuere. Donec vulputate imperdiet hendrerit. Maecenas
+        malesuada vestibulum ornare. Etiam facilisis nibh in neque eleifend, eu
+        aliquet neque cursus. Quisque pretium interdum turpis, non imperdiet
+        tellus molestie ac. Vivamus tempor ipsum enim, quis mattis dolor
+        faucibus non. Vivamus eget ligula a tellus finibus ultricies ut nec
+        felis. Cras nec justo arcu. Ut pharetra scelerisque ultricies. Maecenas
+        mattis nec diam id auctor.{' '}
+      </EpContent>
+      <EpContent>Card Socials and icon</EpContent>
+    </EpMain>
   </Card>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import EpisodeCard from '../components/episodecard'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="End Of The Reel" />
+    <EpisodeCard />
     <h1>End Of The Reel</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
